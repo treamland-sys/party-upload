6f8df83740a7e9619f96bed894c7473f813fd6f0
treamland-sys <treamland@gmail.com>
2026-03-09 14:23:14 +0100

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -1,3 +1,31 @@
-export async function onRequest(context) {
-return new Response("Worker läuft 👍");
+export async function onRequestPost(context) {
+
+const token = "X7fK3RMRtgo8FbA";
+
+const uploadUrl =
+"https://nx70782.your-storageshare.de/public.php/webdav/";
+
+const request = context.request;
+
+const fileName = request.headers.get("x-file-name");
+
+const body = await request.arrayBuffer();
+
+const response = await fetch(uploadUrl + Date.now()+"_"+fileName, {
+
+method:"PUT",
+
+headers:{
+"Authorization":"Basic "+btoa(token+":"),
+"Content-Type":"application/octet-stream"
+},
+
+body:body
+
+});
+
+return new Response(
+"Nextcloud Status: "+response.status
+);
+
 }
