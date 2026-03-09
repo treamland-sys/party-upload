c0d374be758783f1fff5b4c9d487c660fc90721a
treamland-sys <treamland@gmail.com>
2026-03-09 21:49:57 +0100

    Update image.js

--- functions/image.js
+++ functions/image.js
@@ -1,1 +1,23 @@
+export async function onRequestGet(context) {
 
+const url = new URL(context.request.url);
+const file = url.searchParams.get("file");
+
+const shareToken = "X7fK3RMRtgo8FbA";
+
+const nextcloudURL =
+"https://nx70782.your-storageshare.de/public.php/webdav/" + file;
+
+const response = await fetch(nextcloudURL,{
+headers:{
+Authorization:"Basic " + btoa(shareToken + ":")
+}
+});
+
+return new Response(response.body,{
+headers:{
+"Content-Type": response.headers.get("content-type")
+}
+});
+
+}
