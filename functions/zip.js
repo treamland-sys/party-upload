2411870b08ce0c452c79d282b114859f7a5e1e5f
treamland-sys <treamland@gmail.com>
2026-03-10 07:58:13 +0100

    Update zip.js

--- functions/zip.js
+++ functions/zip.js
@@ -1,1 +1,34 @@
+import JSZip from "jszip";
 
+export async function onRequestPost(context) {
+
+const { request } = context;
+const data = await request.json();
+
+const files = data.files;
+
+const zip = new JSZip();
+
+for (const file of files){
+
+const url = "https://party-upload.pages.dev" + file;
+
+const res = await fetch(url);
+const blob = await res.arrayBuffer();
+
+const filename = file.split("file=").pop();
+
+zip.file(filename, blob);
+
+}
+
+const content = await zip.generateAsync({type:"uint8array"});
+
+return new Response(content,{
+headers:{
+"Content-Type":"application/zip",
+"Content-Disposition":"attachment; filename=party_fotos.zip"
+}
+});
+
+}
